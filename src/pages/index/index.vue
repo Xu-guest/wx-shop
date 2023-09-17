927239c53607b7f95a01748caf224ca281f84ef1
Xu-guest <1252867995@qq.com>
2023-09-17 13:22:01 +0800

    轮播图的定义和渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { getBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getBanner = async () => {
+  const res = await getBannerAPI()
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getBanner()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
-  <view class="index">index</view>
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
